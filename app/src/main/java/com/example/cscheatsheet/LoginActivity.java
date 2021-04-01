b9279d7adc90e7d88f374a2f746d24d3bedf988e
jiaruizhong <62999873+jiaruizhong@users.noreply.github.com>
2021-04-01 10:10:46 -0400

    Update LoginActivity.java

--- app/src/main/java/com/example/cscheatsheet/LoginActivity.java
+++ app/src/main/java/com/example/cscheatsheet/LoginActivity.java
@@ -2,77 +2,89 @@ package com.example.cscheatsheet;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 import android.widget.Toast;
-
-import com.example.cscheatsheet.R;
-import com.parse.LogInCallback;
-import com.parse.ParseException;
-import com.parse.ParseUser;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class LoginActivity extends AppCompatActivity {
+import java.util.ArrayList;
 
-    public static final String TAG = "LoginActivity";
-    private EditText etUsername;
-    private EditText etPassword;
-    private Button btnLogin;
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private DBOpenHelper mDBOpenHelper;
+    private TextView mTvLoginactivityRegister;
+    private RelativeLayout mRlLoginactivityTop;
+    private EditText mEtLoginactivityUsername;
+    private EditText mEtLoginactivityPassword;
+    private LinearLayout mLlLoginactivityTwo;
+    private Button mBtLoginactivityLogin;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        if(ParseUser.getCurrentUser() != null){
-            goMainActivity();
-        }
+        initView();
 
-        etUsername = findViewById(R.id.etUsername);
-        etPassword = findViewById(R.id.etUsername);
-        btnLogin= findViewById(R.id.btnLogin);
-
-
-
-        btnLogin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Log.i(TAG, "onClick login button works");
-                String username = etUsername.getText().toString();
-                String password = etPassword.getText().toString();
-                loginUser(username, password);
-            }
-        });
-
+        mDBOpenHelper = new DBOpenHelper(this);
     }
 
-    private void loginUser(String username, String password) {
-        Log.i(TAG, "Attempting to log in user");
-        // Authenticate login information
-        ParseUser.logInInBackground(username, password, new LogInCallback() {
-            @Override
-            public void done(ParseUser user, ParseException e) {
-                if (e != null){
-                    Log.e(TAG, "Login failed!!: ", e);
-                    return;
+    private void initView() {
+        // Initialize the control
+        mBtLoginactivityLogin = findViewById(R.id.bt_loginactivity_login);
+        mTvLoginactivityRegister = findViewById(R.id.tv_loginactivity_register);
+        mRlLoginactivityTop = findViewById(R.id.rl_loginactivity_top);
+        mEtLoginactivityUsername = findViewById(R.id.et_loginactivity_username);
+        mEtLoginactivityPassword = findViewById(R.id.et_loginactivity_password);
+        mLlLoginactivityTwo = findViewById(R.id.ll_loginactivity_two);
+
+        // Set the click event listener
+        mBtLoginactivityLogin.setOnClickListener(this);
+        mTvLoginactivityRegister.setOnClickListener(this);
+    }
+
+    public void onClick(View view) {
+        switch (view.getId()) {
+            // Jump to the registration interface
+            case R.id.tv_loginactivity_register:
+                startActivity(new Intent(this, RegisterActivity.class));
+                finish();
+                break;
+
+            case R.id.bt_loginactivity_login:
+                String name = mEtLoginactivityUsername.getText().toString().trim();
+                String password = mEtLoginactivityPassword.getText().toString().trim();
+                if (!TextUtils.isEmpty(name) && !TextUtils.isEmpty(password)) {
+                    ArrayList<User> data = mDBOpenHelper.getAllData();
+                    boolean match = false;
+                    for (int i = 0; i < data.size(); i++) {
+                        User user = data.get(i);
+                        if (name.equals(user.getName()) && password.equals(user.getPassword())) {
+                            match = true;
+                            break;
+                        } else {
+                            match = false;
+                        }
+                    }
+                    if (match) {
+                        Toast.makeText(this, "Login successful", Toast.LENGTH_SHORT).show();
+                        Intent intent = new Intent(this, MainActivity.class);
+                        startActivity(intent);
+                        finish();// Destroy this Activity
+                    } else {
+                        Toast.makeText(this, "The username or password is incorrect,please enter again", Toast.LENGTH_SHORT).show();
+                    }
+                } else {
+                    Toast.makeText(this, "Please enter your username or password", Toast.LENGTH_SHORT).show();
                 }
-
-                //Navigate to main activity on success
-                goMainActivity();
-                Toast.makeText(LoginActivity.this, "Successfully logged in", Toast.LENGTH_SHORT).show();
-            }
-        });
-
+                break;
+        }
     }
-
-    private void goMainActivity() {
-        Intent i = new Intent(this, MainActivity.class);
-        startActivity(i);
-        finish();
-
-    }
-
 }
