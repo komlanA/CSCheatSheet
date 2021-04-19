910e0684ed366098c1e54533bcc97a384deb7b80
jiaruizhong <62999873+jiaruizhong@users.noreply.github.com>
2021-04-19 12:06:52 -0400

    Update LoginActivity.java

--- app/src/main/java/com/example/cscheatsheet/LoginActivity.java
+++ app/src/main/java/com/example/cscheatsheet/LoginActivity.java
@@ -1,7 +1,9 @@
 package com.example.cscheatsheet;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
