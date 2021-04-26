576f944c124875346a80b529a1dfc3b228a05f30
Komlan Attiogbe <jonathan.attiogbe@gmail.com>
2021-04-26 00:09:52 -0400

    First commit on my own working repo after a fork. Made design changes to color and also set up parse server to handle posting.

--- app/src/main/java/com/example/cscheatsheet/LoginActivity.java
+++ app/src/main/java/com/example/cscheatsheet/LoginActivity.java
@@ -6,6 +6,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
@@ -21,7 +22,7 @@ public class LoginActivity extends AppCompatActivity {
     private EditText etUsername;
     private EditText etPassword;
     private Button btnLogin;
-    private Button btnSignUp;
+    private TextView tvSignUp;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -33,9 +34,9 @@ public class LoginActivity extends AppCompatActivity {
         }
 
         etUsername = findViewById(R.id.etUsername);
-        etPassword = findViewById(R.id.etDescription);
+        etPassword = findViewById(R.id.etPassword);
         btnLogin = findViewById(R.id.btnLogin);
-        btnSignUp = findViewById(R.id.btnSignUp);
+        tvSignUp = findViewById(R.id.tvSignUp);
 
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -47,7 +48,7 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
 
-        btnSignUp.setOnClickListener(new View.OnClickListener() {
+        tvSignUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Log.i(TAG, "onClick sign up button");
